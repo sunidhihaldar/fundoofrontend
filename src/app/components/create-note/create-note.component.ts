53fd17766aa3526adcb3c7ff31fb094de623d124
sunidhihaldar <sunidhihaldar@gmail.com>
2020-03-15 18:39:41 +0530

    [sunidhi]. modify -> dashboard component

--- src/app/components/create-note/create-note.component.ts
+++ src/app/components/create-note/create-note.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { NotesService } from 'src/app/services/notes.service';
+import { NoteModel } from 'src/app/model/note-model';
 
 @Component({
   selector: 'app-create-note',
@@ -14,9 +15,10 @@ export class CreateNoteComponent implements OnInit {
   private expand: boolean = false;
   token: string = localStorage.getItem('token');
   isPinned: boolean;
+  note: NoteModel;
 
   constructor(private noteService: NotesService,    
-              private snackBar: MatSnackBar) {}
+              private matSnackbar: MatSnackBar) {}
   
   onPin() {
     this.isPinned = !this.isPinned;
@@ -46,14 +48,45 @@ export class CreateNoteComponent implements OnInit {
         console.log(response.note);
         this.expand =!this.expand;
         this.noteForm.reset();
-        this.snackBar.open("Succesfully added note", "ok", { duration: 5000 })
+        this.matSnackbar.open("Succesfully added note", "ok", { duration: 5000 })
     },
       error => {
         console.log("error", error);
-        this.snackBar.open("Failed to add note", "ok", { duration: 5000 });
+        this.matSnackbar.open("Failed to add note", "ok", { duration: 5000 });
       }
     );
     }
   }
 
+  onClickArchive() {
+    console.log('Archive note ', this.note.noteId);
+    this.noteService.archiveNote(this.note.noteId).subscribe((response: any) => {
+      console.log('Response ', response);
+      this.matSnackbar.open(response.message, 'Ok', { duration: 5000});
+    },
+    error => {
+      console.log(error);
+      this.matSnackbar.open('Note not archived', 'Ok', { duration: 5000});
+    });
+  }
+
+  updateColour(colour) {
+    console.log('Colour: ', colour);
+    this.noteService.updateColour(this.note.noteId, colour).subscribe((response: any) => {
+      this.matSnackbar.open(response.message, 'Ok', { duration: 5000 });
+    },
+    error => {
+      console.log(error);
+      this.matSnackbar.open('Colour not updated', 'Ok', { duration: 5000});
+    });
+  }
+
+  colors =
+    [
+      [{ name: 'white', value: 'white' }, { name: 'Yellow', value: 'yellow' }, { name: 'lightblue', value: 'lightblue' },],
+    
+      [{ name: 'Brown', value: 'brown' }, { name: 'pink', value: 'pink' }, { name: 'orange', value: 'orange' }, ],
+  
+      [ { name: 'red', value: 'red' }, { name: 'aqua', value: 'aqua' }, { name: 'silver', value: 'silver' }]
+    ];
 }
