612e089e78e7bc4499cf855f85c56476aca1ad99
sunidhihaldar <sunidhihaldar@gmail.com>
2020-03-16 10:02:03 +0530

    [sunidhi].add -> LabelModel class

---
+++ src/app/model/label-model.spec.ts
@@ -0,0 +1,7 @@
+import { LabelModel } from './label-model';
+
+describe('LabelModel', () => {
+  it('should create an instance', () => {
+    expect(new LabelModel()).toBeTruthy();
+  });
+});
---
+++ src/app/model/label-model.ts
@@ -0,0 +1,6 @@
+export class LabelModel {
+    labelId: number;
+    labelName: string;
+    userId: number;
+    noteId: number;
+}
