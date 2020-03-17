fb5d43d4e1ea33cab347fe3b2ebfd53562be35ac
sunidhihaldar <sunidhihaldar@gmail.com>
2020-03-17 16:08:46 +0530

    [sunidhi].add -> onSubmit() functionality in label component

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LabelsService } from 'src/app/services/labels.service';
 import { LabelModel } from 'src/app/model/label-model';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-label',
@@ -13,7 +14,8 @@ export class LabelComponent implements OnInit {
   token = localStorage.getItem('token');
   hitCancel: boolean;
 
-  constructor(private labelService: LabelsService) { }
+  constructor(private labelService: LabelsService,
+              private dialogRef: MatDialogRef<LabelComponent>) { }
 
   ngOnInit() {
   }
@@ -30,6 +32,18 @@ export class LabelComponent implements OnInit {
     error => {
       console.log(error);
     });
+  }
+
+  onSubmit() {
+    this.dialogRef.close();
+    if(this.label.labelName !== '') {
+      this.labelService.createLabel(this.label).subscribe((response: any) => {
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      });
+    }
   }
 
 }
